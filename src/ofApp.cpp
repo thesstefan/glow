6421c7d8290e4f4852ac96d1e55eda7a72c3e1a2
TheMightyStefan <thesstefan@gmail.com>
2019-07-26 23:57:59 +0300

    Add initial version of glow() + input file

--- src/ofApp.cpp
+++ src/ofApp.cpp
@@ -2,35 +2,25 @@
 
 #include "kernel.h"
 #include "image_convolve.h"
+#include "glow.h"
 
 void ofApp::setup(){
-    source.load("img.bmp");
-
     try {
-        GaussianKernel kernel(5, 5, 1);
+        source.load("input.bmp");
+
+        GaussianKernel kernel(25, 25, 10);
 
         kernel.normalize();
 
-        result = convolve(source, kernel);
+        result = glow(source, kernel);
 
-        result = convolve(result, kernel);
-        result = convolve(result, kernel);
-        result = convolve(result, kernel);
-        result = convolve(result, kernel);
+        result.save("output.bmp");
     } catch (const std::exception& exception) {
         std::cerr << exception.what() << std::endl;
     }
 }
 
 void ofApp::draw() {
-    ofFbo fbo;
-
-    fbo.begin();
     result.draw(0, 0);
-    // source.draw(0, 0);
-
-    source.draw(0, source.getHeight() + 20);
-    fbo.end();
-
-    fbo.draw(0, 0);
 }
+
